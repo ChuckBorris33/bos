b6e6ad8e0c47ad34a1378c9e5cf34003a76a2252
Boris Zalman <boris.zalman33@gmail.com>
2026-01-23 22:57:46 +0100

    Add Gluetun VPN container and integrate with fsqd service

--- files/media_server/usr/etc/bos/media_server/config.sh
+++ files/media_server/usr/etc/bos/media_server/config.sh
@@ -66,4 +66,4 @@ http://home.home, http://www.home.home {
 "
 
 export DNSMASQ_DOMAINS="media.home files.home dash.home yt-down.home home.home downloads.home audiobooks.home"
-export SERVICE_PATTERNS="beszel*,dnsmasq*,filebrowser*,jellyfin*,metube*,caddy*,samba*,tailscale*,firewall*,sshd*,fsqd*,audiobookshelf*"
+export SERVICE_PATTERNS="beszel*,dnsmasq*,filebrowser*,jellyfin*,metube*,caddy*,samba*,tailscale*,firewall*,sshd*,fsqd*,audiobookshelf*,gluetun*"
